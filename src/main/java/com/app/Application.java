d5b34578292e849a785a7d7818623f2e42fc8d6b
mithileshtipkari <mithilesh.tipkari@gmail.com>
2020-06-02 23:58:33 +0530

    add: initialize ApplicationContext to fetch required bean

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -7,6 +7,8 @@ package com.app;
 
 import com.app.service.SpeakerService;
 import com.app.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -15,7 +17,11 @@ import com.app.service.SpeakerServiceImpl;
 public class Application {
     
     public static void main(String[] args) {
-        SpeakerService speakerService = new SpeakerServiceImpl();
+//        SpeakerService speakerService = new SpeakerServiceImpl(); //direct hardcoding
+        
+        //set application centext and fetch the required bean by name or class
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        SpeakerService speakerService = appContext.getBean("speakerService", SpeakerService.class);
         System.out.println(speakerService.findAll().get(0).getFirstName());
     }
 }
